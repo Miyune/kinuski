ea90a40a0c35bad0b49e66b29da8141b3210cd3c
Miyune <35492656+Miyune@users.noreply.github.com>
2018-05-14 17:08:59 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,13 +6,13 @@ echo "Asennetaan Salt ja Git"
 sudo apt-get update
 sudo apt-get -y install git salt-minion
 
-echo "Muutetaan asetuksia ja haetaan tilat"
+echo "Muutetaan asetuksia ja haetaan salt-tilat"
 echo 'master: localhost'|sudo tee /etc/salt/minion
 git clone https://github.com/Miyune/kinuski.git
 cd kinuski/
 bash highstate.sh
 
-echo "Desktopin muokkausta"
+echo "Nappaimisto ja aika-asetus vaihdettu FI"
 setxkbmap fi
 sudo timedatectl set-timezone Europe/Helsinki
 git config --global credential.helper "cache --timeout=3600"
